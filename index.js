ec160f431646a6bc8fd3bfa7fead081b40de02f1
SanjeQi <cristi.nord@gmail.com>
2021-08-20 17:54:28 +0100

    React Sample project without npx create-react-app

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log('rock!!! It works like a charm');
+
+// const element = React.createElement('div', null, 'React Element!');
+const element = <div>React element with JSX</div>;
+
+ReactDOM.render(element, document.getElementById('root'));
